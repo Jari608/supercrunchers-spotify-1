eb50475f855f8477030aa9c0c4c2907044b25306
Jari <jari.burgers@gmail.com>
2021-10-27 14:34:59 +0200

    Added json parsing function and CLI parsing implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # supercrunchers-spotify-1
+
+This repository contains a project for the course Supercrunchers at the JADS Den Bosch.
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import requests
+import json
+import argparse
+import pathlib
+
 from bs4 import BeautifulSoup
 
 '''
 ## WHOSAMPLED SCRAPER ##
 
+Author: Jari Burgers
+Course: SuperCrunchers - JADS Den Bosch
+
 to-do:
-    - implement a function that fetches info if n_tracks < 5
     - argparse
     - automatic formating of artist and title
     - implement search function
-
 '''
 
 class Scraper:
@@ -39,49 +44,118 @@ class Scraper:
 
         songs = []
 
-        for page in range(4):
-            print(page)
-            urls = url + str(page)
+        for page in range(8): #only 15 songs per page, iterate over pages
 
+            urls = url + str(page)
             r = self.req.get(urls)
             search_page_soup = BeautifulSoup(r.content, 'html.parser')
 
             for a in search_page_soup.find_all('div'):
+
                 links = a.find_all('a', {'class': 'trackName playIcon'})
                 for link in links:
                     title = link.get('title')
                     songs.append(title)
+
+        return songs
+
+    def fetch_main_page_info(self, url):
+
+        cover  = []
+        remix  = []
+        sample = []
+
+        r = self.req.get(url)
+        search_page_soup = BeautifulSoup(r.content, 'html.parser')
+
+        for a in search_page_soup.find_all('div'):
+            links = a.find_all('a', {'class': 'trackName playIcon'})
+            for link in links:
+                title = link.get('title')
+                href = link.get('href')
+
+                if href.startswith('/sample'):
+                    sample.append(title)
+                elif href.startswith('/remix'):
+                    remix.append(title)
+                elif href.startswith('/cover'):
+                    cover.append(title)
+                else:
+                    print('source unknown')
+
+        return  cover, remix, sample
         
-        return set(songs)
 
     def get_tracks(self, artist, title):
-        types = ['covered', 'remixed', 'sampled']
-        response = []
-        for type in types:
-            url = f'https://www.whosampled.com/{artist}/{title}/{type}/?cp='.format(artist = artist, title = title, type = type)
 
-            response.append(s.fetch(url))
+        response_cov = []
+        response_rem = []
+        response_sam = []
 
-        return response[0], response[1], response[2]
+        url_main = f'https://www.whosampled.com/{artist}/{title}/'.format(artist = artist, title = title)
+        cover, remix, sample = s.fetch_main_page_info(url_main)
+        response_cov.append(cover)
+        response_rem.append(remix)
+        response_sam.append(sample)
+
+        url = f'https://www.whosampled.com/{artist}/{title}/covered/?cp='.format(artist = artist, title = title)
+        response_cov.append(s.fetch(url))
+
+        url = f'https://www.whosampled.com/{artist}/{title}/remixed/?cp='.format(artist = artist, title = title)
+        response_rem.append(s.fetch(url))
+
+        url = f'https://www.whosampled.com/{artist}/{title}/sampled/?cp='.format(artist = artist, title = title)
+        response_sam.append(s.fetch(url))
+
+        cov = set([item for sublist in response_cov for item in sublist])
+        rem = set([item for sublist in response_rem for item in sublist])
+        sam = set([item for sublist in response_sam for item in sublist])
+
+        return cov, rem, sam
     
+    def format_input(self, input):
+        output = input
+        return output
+
     def generate_output(self, artist, title):
         
         covers, remixes, sampled = s.get_tracks(artist, title)
 
-        print("{} - {}".format(artist, title))
-        print(str(len(covers)) + " Covers\n")
-        print(covers)
-        print("\n")
-        print(str(len(remixes)) + " Remixes\n")
-        print(remixes)
-        print("\n")
-        print("Sampled in {} tracks\n". format(len(sampled)))
-        print(sampled)
-        print("\n")
+        data = {}
+        data['Artist'] = artist
+        data['title'] = title
+        data['Metrics'] = []
+        data['Metrics'].append({
+            'n_covers': len(covers),
+            'n_remixes': len(remixes),
+            'n_sampled': len(sampled) 
+        })
+        data['covers'] = list(covers)
+        data['remixes'] = list(remixes)
+        data['sampled'] = list(sampled)
+
+        with open("Data/{}_{}.json".format(artist, title), "w") as f:
+            json.dump(data, f)
+
+        print(json.dumps(data, indent = 4))
+
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, 
+        description = "A tool which retrieves all samples, remixes and covers for a given song from WhoSampled.com",
+        epilog = "Example call: python main.py Donna-Summer I-Feel-Love") #default will be shown using the --help command
+
+    parser.add_argument("artist", type = str, help = "Artist of the song")
+    parser.add_argument("title", type = str, help = "Title of the song")
+    parser.add_argument("--verbose", type = str, help = "print the output (it will always be saved to a json file in the /Data folder)")
+    args = parser.parse_args()
+
     s = Scraper(3)
 
-    s.generate_output('Donna-Summer', 'I-Feel-Love')
-    s.generate_output('Elvis-Presley', 'A-Little-Less-Conversation')
+    pathlib.Path("Data/").mkdir(exist_ok = True)
+
+    s.generate_output(args.artist, args.title)
+
+    #s.generate_output('Donna-Summer', 'I-Feel-Love')
+    #s.generate_output('Elvis-Presley', 'A-Little-Less-Conversation')
